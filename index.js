1bf0bdba68b462b76505cbed7f8742a3cc75f5a7
santosh <santosh220903@gmail.com>
2025-04-13 15:53:33 +0530

    revert something

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const name = 'santosh sharma'
 const dob = '22/09/2003'
 
 function sayName() {
-	console.log('My name is ' + name1);
+	console.log('My name is ' + name);
 	
+<<<<<<< HEAD
 }
 
 function newFun(){
 	console.log('My name is sk')
+=======
+>>>>>>> parent of 38196c7 (added is above18)
 }
