a949bacfd371f2acad66ee4f2869ee5756681414
santosh <santosh220903@gmail.com>
2025-06-21 01:20:26 +0530

    3rd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 // Serve static files (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
