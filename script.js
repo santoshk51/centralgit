27aec508a3045279a0ac808e5280fb452b87d252
santosh <santosh220903@gmail.com>
2025-07-21 00:00:31 +0530

    2 changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// var typed =  new Typed(".text", {
-// 	strings:["Devops Engineer" , "Cloud Engineer" , "Web Developer"],
-// 	typeSpeed:50,
-// 	backSpeed:50,
-// 	loop:true
-// });
+var typed =  new Typed(".text", {
+	strings:["Devops Engineer" , "Cloud Engineer" , "Web Developer"],
+	typeSpeed:50,
+	backSpeed:50,
+	loop:true
+});
